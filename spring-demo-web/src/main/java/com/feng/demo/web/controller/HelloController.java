d249611e7f53e0bd9194da16580ded88f84bac3b
FengYD <f18813108509@gmail.com>
2022-04-22 14:42:44 +0800

    add test

--- spring-demo-web/src/main/java/com/feng/demo/web/controller/HelloController.java
+++ spring-demo-web/src/main/java/com/feng/demo/web/controller/HelloController.java
@@ -4,8 +4,12 @@ import com.feng.demo.model.vo.BaseVo;
 import com.feng.demo.web.aspect.AuthToken;
 import com.feng.demo.web.aspect.TimeWatch;
 import com.feng.demo.web.config.datasource.DynamicDataSourceContextHolder;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author fengyadong
@@ -22,6 +26,18 @@ public class HelloController {
     public BaseVo hello() {
         DynamicDataSourceContextHolder.getContextKey();
         return BaseVo.success("Hello World!");
+    }
+
+    @RequestMapping("/hello2")
+    public BaseVo hello2() {
+        List<String> sss = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            sss.add(null);
+        }
+        if (CollectionUtils.isEmpty(sss)) {
+            System.out.println("aaa");
+        }
+        return BaseVo.success();
     }
 
 }
