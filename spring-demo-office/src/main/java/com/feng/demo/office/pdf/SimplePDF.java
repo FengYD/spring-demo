cbf4579e83fdab410f3901a1d8bfc1c50e2a62a3
FengYD <f18813108509@gmail.com>
2022-04-22 17:35:10 +0800

    pdf

--- spring-demo-office/src/main/java/com/feng/demo/office/pdf/SimplePDF.java
+++ spring-demo-office/src/main/java/com/feng/demo/office/pdf/SimplePDF.java
@@ -1,8 +1,12 @@
 package com.feng.demo.office.pdf;
 
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.List;
+import com.itextpdf.layout.element.ListItem;
 import com.itextpdf.layout.element.Paragraph;
 
 /**
@@ -13,7 +17,7 @@ import com.itextpdf.layout.element.Paragraph;
 public class SimplePDF {
 
     public static void main(String[] args) throws Exception {
-        sample1();
+        sample2();
     }
 
 
@@ -27,11 +31,28 @@ public class SimplePDF {
     }
 
     public static void sample2() throws Exception {
-        String dest = "/Users/fengyadong/Desktop/sample1.pdf";
+        String dest = "/Users/fengyadong/Desktop/sample2.pdf";
         PdfWriter writer = new PdfWriter(dest);
         PdfDocument pdf = new PdfDocument(writer);
         Document document = new Document(pdf);
-        document.add(new Paragraph("Hello World!"));
+        // Create a PdfFont
+        PdfFont font = PdfFontFactory.createFont();
+        // Add a Paragraph
+        document.add(new Paragraph("iText is:").setFont(font));
+        // Create a List
+        List list = new List()
+                .setSymbolIndent(12)
+                .setListSymbol("\u2022")
+                .setFont(font);
+        // Add ListItem objects
+        list.add(new ListItem("Never gonna give you up"))
+                .add(new ListItem("Never gonna let you down"))
+                .add(new ListItem("Never gonna run around and desert you"))
+                .add(new ListItem("Never gonna make you cry"))
+                .add(new ListItem("Never gonna say goodbye"))
+                .add(new ListItem("Never gonna tell a lie and hurt you"));
+        // Add the list
+        document.add(list);
         document.close();
     }
 }
