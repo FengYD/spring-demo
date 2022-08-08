8235f19a9cb149e9d5aaa13c61b385a4329267c9
FenfYD <f18813108509@gmail.com>
2022-08-08 09:29:17 +0800

    去除apollo依赖

--- spring-demo-web/src/main/java/com/feng/demo/Application.java
+++ spring-demo-web/src/main/java/com/feng/demo/Application.java
@@ -1,6 +1,5 @@
 package com.feng.demo;
 
-import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +10,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @date 2022/3/8 5:57 下午
  * @description
  */
-@EnableApolloConfig
 @MapperScan(basePackages = "com.feng.demo.*.mapper")
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Application {
