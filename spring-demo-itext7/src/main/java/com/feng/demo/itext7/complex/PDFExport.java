352dd8eb14d62c8aabf0bb018782d3d07e6f463d
fengyadong <f18813108509@gmail.com>
2023-04-24 00:49:17 +0800

    完成

--- spring-demo-itext7/src/main/java/com/feng/demo/itext7/complex/PDFExport.java
+++ spring-demo-itext7/src/main/java/com/feng/demo/itext7/complex/PDFExport.java
@@ -6,20 +6,24 @@ import com.itextpdf.kernel.geom.PageSize;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.AreaBreak;
+import com.itextpdf.layout.element.Cell;
 import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
 import com.itextpdf.layout.properties.TextAlignment;
+import com.itextpdf.layout.properties.VerticalAlignment;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * @author fengyadong
  * @date 2023/4/23 16:42
  * @Description
  */
+@Slf4j
 public class PDFExport {
 
     private static final String Song = "宋体";
@@ -31,6 +35,7 @@ public class PDFExport {
     private static PdfFont fontHuaWen;
 
     public static void main(String[] args) throws IOException {
+        long startTime = System.currentTimeMillis();
         loadFonts();
         AssistLetterExportModel model = buildModel();
         String dest = "spring-demo-itext7/results/" + model.getSendManDeptName() + "（" + model.getAssistLetterNo() + "）.pdf";
@@ -42,11 +47,112 @@ public class PDFExport {
         document.setBottomMargin(96);
         document.setLeftMargin(79);
         document.setRightMargin(74);
-        Paragraph header1 = new Paragraph(model.getSendManDeptName()).setFont(fontSong).setFontSize(16).setTextAlignment(TextAlignment.CENTER);
-        Paragraph header2 = new Paragraph("协助提供数据函").setFont(fontSong).setFontSize(16).setTextAlignment(TextAlignment.CENTER);
+        Paragraph header1 = new Paragraph(model.getSendManDeptName()).setFont(fontSong).setFontSize(26).setTextAlignment(TextAlignment.CENTER);
+        Paragraph header2 = new Paragraph("协助提供数据函").setFont(fontSong).setFontSize(26).setTextAlignment(TextAlignment.CENTER);
         Paragraph assistLetterNo = new Paragraph(model.getSendManDeptName()).setFont(fontFangSong).setFontSize(14).setTextAlignment(TextAlignment.RIGHT);
         document.add(header1).add(header2).add(assistLetterNo);
+
+        Paragraph p1 = new Paragraph(model.getAssistPlatEntityName()).setFont(fontFangSong).setFontSize(16).setTextAlignment(TextAlignment.LEFT);
+        Paragraph p2 = new Paragraph(model.getAssistReason())
+                .setFont(fontFangSong).setFontSize(16)
+                .setTextAlignment(TextAlignment.LEFT)
+                .setFirstLineIndent(32);
+        Paragraph p3 = new Paragraph("涉案信息如下：")
+                .setFont(fontFangSong).setFontSize(16)
+                .setTextAlignment(TextAlignment.LEFT)
+                .setFirstLineIndent(32);
+        Paragraph p4 = new Paragraph("查询范围：" + model.getAssistScopeName())
+                .setFont(fontFangSong).setFontSize(16)
+                .setTextAlignment(TextAlignment.LEFT)
+                .setFirstLineIndent(32);
+        document.add(p1).add(p2).add(p3).add(p4);
+        for (AssistLetterExportModel.KVData kvData : model.getAssistProvideDataVO()) {
+            Paragraph p = new Paragraph(kvData.getKey() + "：" + kvData.getValue())
+                    .setFont(fontFangSong).setFontSize(16)
+                    .setTextAlignment(TextAlignment.LEFT)
+                    .setFirstLineIndent(32);
+            document.add(p);
+        }
+        Paragraph p5 = new Paragraph("请你单位在收到本函后予以协助，于" + model.getAssistLimit() +
+                "内（一般为收到函件的10个工作日）内反馈调查结果。（反馈调查结果数据见附件1）需要延期完成的，请在期限届满" +
+                "前告知本局。请提供以上事项的相关数据信息，并及时将结果回复至" + model.getSendManDeptName() + "。")
+                .setFont(fontFangSong).setFontSize(16)
+                .setTextAlignment(TextAlignment.LEFT)
+                .setFirstLineIndent(32);
+        Paragraph p6 = new Paragraph("联系人：" + model.getSendUserName())
+                .setFont(fontFangSong).setFontSize(16)
+                .setTextAlignment(TextAlignment.LEFT)
+                .setFirstLineIndent(32);
+        Paragraph p7 = new Paragraph("联系电话：" + model.getSendUserPhone())
+                .setFont(fontFangSong).setFontSize(16)
+                .setTextAlignment(TextAlignment.LEFT)
+                .setFirstLineIndent(32);
+        document.add(p5).add(p6).add(p7);
+        Paragraph p8 = new Paragraph(model.getSendManDeptName())
+                .setFont(fontFangSong).setFontSize(16)
+                .setTextAlignment(TextAlignment.RIGHT);
+        Paragraph p9 = new Paragraph("（盖章）")
+                .setFont(fontFangSong).setFontSize(16)
+                .setTextAlignment(TextAlignment.RIGHT);
+        Paragraph p10 = new Paragraph(model.getSendTime())
+                .setFont(fontFangSong).setFontSize(16)
+                .setTextAlignment(TextAlignment.RIGHT);
+        document.add(p8).add(p9).add(p10);
+        // 分页
+        document.add(new AreaBreak());
+        Paragraph p11 = new Paragraph("附件1")
+                .setFont(fontFangSong).setFontSize(16)
+                .setTextAlignment(TextAlignment.LEFT);
+        document.add(p11);
+        for (int i = 1; i <= model.getAssistReplyDataVO().size(); i++) {
+            AssistLetterExportModel.AssistPlatScopeVO vo = model.getAssistReplyDataVO().get(i - 1);
+            Paragraph tableName = new Paragraph(i + "." + vo.getAssistScopeName())
+                    .setFont(fontFangSong).setFontSize(16)
+                    .setTextAlignment(TextAlignment.LEFT)
+                    .setFirstLineIndent(32);
+            document.add(tableName);
+            Table table = new Table(new float[]{4, 6}).useAllAvailableWidth();
+            Paragraph tableHeader1 = new Paragraph("数据类别").setFont(fontHuaWen).setFontSize(16);
+            table.addHeaderCell(new Cell().add(tableHeader1));
+            Paragraph tableHeader2 = new Paragraph("具体数据项").setFont(fontHuaWen).setFontSize(16);
+            table.addHeaderCell(new Cell().add(tableHeader2));
+            Map<String, List<String>> tempMap = new LinkedHashMap<>();
+            for (AssistLetterExportModel.AssistPlatReplyVO replyVO : vo.getAssistPlatReplyVOList()) {
+                if (tempMap.containsKey(replyVO.getAssistReplyType())) {
+                    tempMap.get(replyVO.getAssistReplyType()).add(replyVO.getAssistReplyName());
+                } else {
+                    List<String> tempList = new ArrayList<>();
+                    tempList.add(replyVO.getAssistReplyName());
+                    tempMap.put(replyVO.getAssistReplyType(), tempList);
+                }
+            }
+            for (String tempKey : tempMap.keySet()) {
+                List<String> tempList = tempMap.get(tempKey);
+                Paragraph type = new Paragraph(tempKey)
+                        .setFont(fontFangSong).setFontSize(16)
+                        .setTextAlignment(TextAlignment.LEFT);
+                Cell cell = null;
+                if (tempList.size() > 1) {
+                    cell = new Cell(tempList.size(), 1).add(type).setVerticalAlignment(VerticalAlignment.MIDDLE);
+                } else {
+                    cell = new Cell().add(type).setVerticalAlignment(VerticalAlignment.MIDDLE);
+                }
+                table.addCell(cell);
+                tempList.forEach(
+                        e -> {
+                            Paragraph name = new Paragraph(tempKey)
+                                    .setFont(fontFangSong).setFontSize(16)
+                                    .setTextAlignment(TextAlignment.LEFT);
+                            Cell cellName = new Cell().add(name).setVerticalAlignment(VerticalAlignment.MIDDLE);
+                            table.addCell(cellName);
+                        }
+                );
+            }
+            document.add(table);
+        }
         pdf.close();
+        long endTime = System.currentTimeMillis();
+        log.info("cost time {} ms", endTime - startTime);
     }
 
     private static void loadFonts() throws IOException {
@@ -80,21 +186,21 @@ public class PDFExport {
         AssistLetterExportModel.KVData kvData = new AssistLetterExportModel.KVData("联系人", "1111");
         model.getAssistProvideDataVO().add(kvData);
         AssistLetterExportModel.AssistPlatReplyVO vo = new AssistLetterExportModel.AssistPlatReplyVO(
-                null, "贝贝网的文件系统管理员贝贝网的文件", "贝贝网的文件");
+                null, "贝贝网的文件系统管理员贝贝网的TYPE1", "贝贝网的文件");
         AssistLetterExportModel.AssistPlatReplyVO vo1 = new AssistLetterExportModel.AssistPlatReplyVO(
-                null, "贝贝网的文件系统管理员贝贝网的文件", "贝贝网的文件贝贝网");
+                null, "贝贝网的文件系统管理员贝贝网的TYPE1", "贝贝网的文件贝贝网");
         AssistLetterExportModel.AssistPlatReplyVO vo2 = new AssistLetterExportModel.AssistPlatReplyVO(
-                null, "贝贝网的文件系统管理员贝贝网的文件", "贝贝网的文");
+                null, "贝贝网的文件系统管理员贝贝网的TYPE1", "贝贝网的文");
         AssistLetterExportModel.AssistPlatReplyVO vo3 = new AssistLetterExportModel.AssistPlatReplyVO(
-                null, "贝贝网的文件系统管理员贝贝网的文件", "贝贝网的");
+                null, "贝贝网的文件系统管理员贝贝网的TYPE1", "贝贝网的");
         AssistLetterExportModel.AssistPlatReplyVO vo4 = new AssistLetterExportModel.AssistPlatReplyVO(
-                null, "贝贝网的文件贝贝", "贝贝网的文件");
+                null, "贝贝网的文件贝贝TYPE2", "贝贝网的文件");
         AssistLetterExportModel.AssistPlatReplyVO vo5 = new AssistLetterExportModel.AssistPlatReplyVO(
-                null, "贝贝网的文件贝贝", "贝贝网的");
+                null, "贝贝网的文件贝贝TYPE3", "贝贝网的");
         AssistLetterExportModel.AssistPlatReplyVO vo6 = new AssistLetterExportModel.AssistPlatReplyVO(
-                null, "贝贝网的文件贝贝", "贝贝网的");
+                null, "贝贝网的文件贝贝TYPE3", "贝贝网的");
         AssistLetterExportModel.AssistPlatReplyVO vo7 = new AssistLetterExportModel.AssistPlatReplyVO(
-                null, "贝贝网的文件贝贝", "贝贝网的文件");
+                null, "贝贝网的文件贝贝TYPE3", "贝贝网的文件");
         List<AssistLetterExportModel.AssistPlatReplyVO> list = new ArrayList<>();
         list.add(vo);
         list.add(vo1);
